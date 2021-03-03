c05f341569a028052bd9b6acad947d4af9a58b06
tt <tt@DESKTOP-1PBCNG6>
2021-03-03 19:38:44 +0800

    Homework1

---
+++ Homework1/Homework1-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Homework1_1
+{
+    class Program
+    {
+        public static double Trans()//turns string to double
+        {
+            Console.Write("Enter number:");
+            string s = Console.ReadLine();
+            return Convert.ToDouble(s);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an arithmatic expression '+' '-' '*' '/'");
+            Console.WriteLine("Enter 2 Numbers");
+            double a, b;
+            a = Trans();
+            b = Trans();
+            loop:
+            Console.WriteLine("Enter the Operation:");
+            int c = Console.Read();
+            if (c == '+')
+            {
+                Console.WriteLine("{0} {1} {2} = {3}", a, Convert.ToChar(c), b, a + b);
+            }else if (c == '-')
+            {
+                Console.WriteLine("{0} {1} {2} = {3}", a, Convert.ToChar(c), b, a - b);
+            }else if (c == '*')
+            {
+                Console.WriteLine("{0} {1} {2} = {3}", a, Convert.ToChar(c), b, a * b);
+            }else if (c == '/')
+            {
+                Console.WriteLine("{0} {1} {2} = {3}", a, Convert.ToChar(c), b, a / b);
+            }
+            else
+            {
+                Console.WriteLine("Operation error input again!");
+                int e=Console.Read();
+                int f=Console.Read();
+                int g=Console.Read();//get console left char
+                goto loop;
+            }
+        }
+    }
+}
---
+++ Homework1/Homework1-2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackgroundImage = Image.FromFile("C:\\Users\\24356\\Desktop\\Software Constuction\\SC Homework1 2019302120118 qh\\Homework1\\pic\\background.jpg");
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double numberA = Convert.ToDouble(textBox1.Text);
+            double numberB = Convert.ToDouble(textBox2.Text);
+            string s = comboBox1.Text;
+            switch(s)
+            {
+                case "+":
+                    textBox3.Text = (numberA + numberB).ToString();
+                    break;
+                case "-":
+                    textBox3.Text = (numberA - numberB).ToString();
+                    break;
+                case "*":
+                    textBox3.Text = (numberA * numberB).ToString();
+                    break;
+                case "/":
+                    if (numberB == 0)
+                        MessageBox.Show("The divider cant be 0.", "Error", MessageBoxButtons.OK);
+                    else
+                        textBox3.Text = (numberA / numberB).ToString();
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.comboBox1.Items.Add("+");
+            this.comboBox1.Items.Add("-");
+            this.comboBox1.Items.Add("*");
+            this.comboBox1.Items.Add("/");
+            this.comboBox1.SelectedIndex = 0;
+            this.Text = "Calculater";
+        }
+    }
+}
