4ebbc049cd466b83aafff934e6933f053dea4a8d
tt <tt@DESKTOP-1PBCNG6>
2021-03-09 18:15:02 +0800

    3

---
+++ Homework3/Homework3-1/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Homework3_1
+{
+    public interface Shape
+    {
+        double area();
+        void determine();
+    }
+
+    public class Rectangle: Shape
+    {
+        double width, height;
+        public Rectangle() 
+        {
+            width = 5;
+            height = 3;
+        }
+        public Rectangle(double w, double h)
+        {
+            width = w;
+            height = h;
+        }
+        public double area()
+        {
+            Console.WriteLine("Area = {0}", width * height);
+            return width * height;
+        }
+        public void determine()
+        {
+            if (width > 0 && height > 0)
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+        }
+    }
+
+    public class Square: Rectangle
+    {
+        public Square(double a): base(a, a)
+        {
+            
+        }
+        public Square() : this(5)
+        {
+            
+        }
+    }
+
+    public class Triangle: Shape
+    {
+        double a, b, c;
+        public Triangle(): this(1)
+        {
+            
+        }
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public Triangle(double a)
+        {
+            this.a = a;
+            b = a;
+            c = a;
+        }
+        public double area()
+        {
+            double p = (a + b + c) / 2;
+            double area = System.Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine("Area = {0}", area);
+            return area;
+        }
+        public void determine()
+        {
+            if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a )
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rectangle:");
+            Shape a = new Rectangle(2, 2);
+            a.area();
+            a.determine();
+            a = new Rectangle(0, 2);
+            a.determine();
+            //------------
+            Console.WriteLine("Square:");
+            Shape b = new Square(2);
+            b.area();
+            b.determine();
+            //------------
+            Console.WriteLine("Triangle:");
+            Shape c = new Triangle(1, 2, 3);
+            c.determine();
+            c = new Triangle(2, 2, 3);
+            c.area();
+            c.determine();
+            c = new Triangle(1);
+            c.area();
+            c.determine();
+        }
+    }
+}
---
+++ Homework3/Homework3-2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Homework3_1;
+
+namespace Homework3_2
+{
+    /*enum ShapeType
+    {
+        Rectangle = 0,
+        Square = 1,
+        Triangle = 2
+    }*/
+    class ShapeFactory
+    {
+        public Shape GetShape(int shapeType)
+        {
+            if (shapeType == 0)
+            {
+                Console.WriteLine("Rectangle: 5*3");
+                return new Rectangle();
+            }
+            else if (shapeType == 1)
+            {
+                Console.WriteLine("Square: 5*5");
+                return new Square();
+            }
+            else if (shapeType == 2)
+            {
+                Console.WriteLine("Triangle: 1 1 1");
+                return new Triangle();
+            }
+            else
+            {
+                Console.WriteLine("Null");
+                return null;
+            }
+
+        }
+    }
+    class Program
+    {
+        public static int GetRandomNumber()
+        {
+            Random r = new Random();
+            int i = r.Next(0, 3);
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            double area = 0;
+            ShapeFactory sf = new ShapeFactory();
+            for(int i = 0; i < 10; i++)
+            {
+                Shape s = sf.GetShape(GetRandomNumber());
+                area += s.area();
+            }
+            Console.WriteLine("Total Area = {0}", area);
+        }
+    }
+}
